f734e656a705294bceede2f8f1bc89a40fc538ab
hayasilin <harvey.hayasi@gmail.com>
2016-08-27 18:29:17 +0800

    Add statusBarLightConent.m

---
+++ Objc/Interface/statusBarLightContent.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Component
+//
+//  Created by Kuan-Wei Lin on 6/18/16.
+//  Copyright © 2016 Kuan-Wei Lin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+}
+
+//-(UIStatusBarStyle)preferredStatusBarStyle{
+//    return UIStatusBarStyleLightContent;
+//}
+
+@end
